c9db28f2a5c310796aa575db38359a57b391f6ab
bboyle <bboyle@email.arizona.edu>
2019-05-29 14:39:26 -0700

    Set NULL to '' in output of nsr_ws and nsr_wsb

--- nsr_batch_includes/dump_nsr_results.php
+++ nsr_batch_includes/dump_nsr_results.php
@@ -16,7 +16,7 @@ However, better than the granting FILE permission.
 For possible solutions, see:
 https://stackoverflow.com/questions/20751352/suppress-warning-messages-using-mysql-from-within-terminal-but-password-written
 */
-$cmd="mysql -u $USERW --password=$PWDW -B $DB_BATCH -e \"select id, family, IF(genus LIKE '%aceae','',genus) AS genus, IF(species NOT LIKE '% %','',species) AS species, country, state_province, county_parish, poldiv_full, poldiv_type, native_status_country, native_status_state_province, native_status_county_parish, native_status, native_status_reason, native_status_sources, isIntroduced, isCultivatedNSR, is_cultivated_taxon, user_id from observation where $JOB_WHERE_NA \" > $resultsfile";
+$cmd="mysql -u $USERW --password=$PWDW -B $DB_BATCH -e \"select id, family, IF(genus LIKE '%aceae','',genus) AS genus, IF(species NOT LIKE '% %','',species) AS species, country, IFNULL(state_province,'') AS state_province, IFNULL(county_parish,'') AS county_parish, poldiv_full, poldiv_type, native_status_country, native_status_state_province, native_status_county_parish, native_status, native_status_reason, native_status_sources, isIntroduced, isCultivatedNSR, is_cultivated_taxon, user_id from observation where $JOB_WHERE_NA \" > $resultsfile";
 //echo "cmd:\n$cmd\n";
 exec($cmd);
 
